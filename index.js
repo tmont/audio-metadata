a39e6e953becc51667552c6e8731cf1af8742076
tmont <tommy.mont@gmail.com>
2014-01-08 20:36:53 -0800

    support for id3v2.4

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function readBytes(view, offset, length, target) {
 	return bytes;
 }
 
-function readString(view, offset, length) {
+function readUtf8(view, offset, length) {
 	var buffer = view.buffer.slice(offset, offset + length);
 
 	//http://stackoverflow.com/a/17192845 - convert byte array to UTF8 string
@@ -28,11 +28,20 @@ function readString(view, offset, length) {
 	return decodeURIComponent(escape(encodedString));
 }
 
-/**
- * See http://www.ietf.org/rfc/rfc3533.txt
- * @param {Buffer|ArrayBuffer} buffer
- */
-exports.ogg = function(buffer) {
+function readAscii(view, offset, length) {
+	var s = '';
+	for (var i = 0; i < length; i++) {
+		s += String.fromCharCode(view.getUint8(offset + i));
+	}
+
+	return s;
+}
+
+function normalizeCommentKey(key) {
+	return key.toLowerCase();
+}
+
+function getArrayBuffer(buffer) {
 	if (typeof(Buffer) !== 'undefined' && buffer instanceof Buffer) {
 		//convert nodejs buffers to ArrayBuffer
 		buffer = toArrayBuffer(buffer);
@@ -42,7 +51,15 @@ exports.ogg = function(buffer) {
 		throw new Error('Expected instance of Buffer or ArrayBuffer');
 	}
 
-	var view = new DataView(buffer);
+	return buffer;
+}
+
+/**
+ * See http://www.ietf.org/rfc/rfc3533.txt
+ * @param {Buffer|ArrayBuffer} buffer
+ */
+exports.ogg = function(buffer) {
+	var view = new DataView(getArrayBuffer(buffer));
 
 	function parsePage(offset, withPacket) {
 		var numPageSegments = view.getUint8(offset + 26),
@@ -71,10 +88,10 @@ exports.ogg = function(buffer) {
 
 		for (var i = 0; i < commentListLength; i++) {
 			var commentLength = packet.getUint32(offset, true),
-				comment = readString(packet, offset + 4, commentLength),
+				comment = readUtf8(packet, offset + 4, commentLength),
 				equals = comment.indexOf('=');
 
-			comments[comment.substring(0, equals).toLowerCase()] = comment.substring(equals + 1);
+			comments[normalizeCommentKey(comment.substring(0, equals))] = comment.substring(equals + 1);
 			offset += 4 + commentLength;
 		}
 
@@ -82,4 +99,124 @@ exports.ogg = function(buffer) {
 	}
 
 	return parseComments(parsePage(parsePage(0).pageSize, true).packet);
+};
+
+function checkMagicId3(view, offset) {
+	var id3Magic = readBytes(view, offset, 3);
+	//"ID3"
+	return id3Magic[0] === 73 && id3Magic[1] === 68 && id3Magic[2] === 51;
+}
+
+function getUint28(view, offset) {
+	var sizeBytes = readBytes(view, offset, 4);
+	var mask = 0xfffffff;
+	return ((sizeBytes[0] & mask) << 21) |
+		((sizeBytes[1] & mask) << 14) |
+		((sizeBytes[2] & mask) << 7) |
+		(sizeBytes[3] & mask);
+}
+
+exports.id3 = function(buffer) {
+	var view = new DataView(buffer);
+	if (checkMagicId3(view, 0)) {
+		return exports.id3v2(buffer);
+	}
+
+	//TODO id3v1
+	return {};
+};
+
+exports.id3v2 = function(buffer) {
+	var view = new DataView(getArrayBuffer(buffer));
+	if (!checkMagicId3(view, 0)) {
+		throw new Error('Magic ID3 failed');
+	}
+
+	var offset = 3;
+	var majorVersion = view.getUint8(offset);
+	var minorVersion = view.getUint8(offset + 1);
+	offset += 2;
+	var flags = view.getUint8(offset);
+	offset++;
+	var size = getUint28(view, offset);
+	offset += 4;
+
+	var unsynchronization = (flags & 256) > 0;
+	var extendedHeader = (flags & 128) > 0;
+	var experimental = (flags & 64) > 0;
+	var footerPresent = (flags & 32) > 0;
+
+	if (extendedHeader) {
+		var ehSize = getUint28(view, offset);
+		offset += 4;
+		var ehNumFlags = view.getUint8(offset);
+		offset++;
+		var ehFlags = view.getUint8(offset);
+		offset++;
+		var ehData = readBytes(view, offset, ehSize - 6);
+	}
+
+	function readFrame(offset) {
+		var id = readAscii(view, offset, 4);
+		offset += 4;
+		var size = getUint28(view, offset);
+		offset += 4;
+		var flags = view.getUint16(view, offset);
+		offset += 2;
+
+		var encoding = view.getUint8(offset),
+			data = null;
+
+		if (encoding <= 3) {
+			offset++;
+			if (encoding === 3) {
+				//UTF8 - null terminated
+				data = readUtf8(view, offset, size - 2);
+			} else {
+				//ISO-8859-1, UTF-16, UTF-16BE
+				//UTF-16 and UTF-16BE are $FF $00 terminated
+				//ISO is null terminated
+
+				//screw these encodings, read it as ascii
+				data = readAscii(view, offset, size - (encoding === 0 ? 2 : 3));
+			}
+		} else {
+			//no encoding info, read it as ascii
+			data = readAscii(view, offset, size);
+		}
+
+		return {
+			id: id,
+			size: size + 10,
+			content: data
+		};
+	}
+
+	var idMap = {
+		TALB: 'album',
+		TCOM: 'composer',
+		TIT1: 'title',
+		TIT2: 'title',
+		TPE1: 'artist',
+		TRCK: 'track',
+		TSSE: 'encoder',
+		TCOP: 'year'
+	};
+
+	var endOfTags = offset + size,
+		frames = {};
+	while (offset < endOfTags) {
+		var frame = readFrame(offset);
+		offset += frame.size;
+		var id = idMap[frame.id] || frame.id;
+		if (id === 'TXXX') {
+			var nullByte = frame.content.indexOf('\u0000');
+			id = frame.content.substring(0, nullByte);
+			frames[id] = frame.content.substring(nullByte + 1);
+		} else {
+			frames[id] = frame.content;
+		}
+	}
+
+	return frames;
 };
--- tests/metadata-tests.js
+++ tests/metadata-tests.js
@@ -24,3 +24,25 @@ describe('ogg', function() {
 		});
 	});
 });
+
+describe('id3', function() {
+	it('should read id3v2.4.0', function(done) {
+		var file = path.join(__dirname, 'files', 'test_id3v2.4.mp3');
+		fs.readFile(file, function(err, buffer) {
+			if (err) {
+				done(err);
+				return;
+			}
+
+			var metadata = metaDataReader.id3v2(buffer);
+			should.exist(metadata);
+			metadata.should.have.property('title', 'Contra');
+			metadata.should.have.property('artist', 'Bill & Ted');
+			metadata.should.have.property('album', 'Konami');
+			metadata.should.have.property('year', '2000');
+			metadata.should.have.property('encoder', 'Lavf53.21.1');
+			metadata.should.have.property('track', '2');
+			done();
+		});
+	});
+});
